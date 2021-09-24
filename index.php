03310c2e159af945a988060c1d8f739c08639b6d
jwizzle29 <joshgg29@gmail.com>
2021-09-24 21:17:23 +0000

    ffs

--- index.php
+++ index.php
@@ -82,7 +82,8 @@ foreach($products as $item){
     foreach($strainInfo[$item->getCName()]->data->filteredProducts->products as $sdata){
         echo "Description : " .$sdata->description . "<br>";      
     }
-    $item->setDescription($sdata->description);
+    $stripped = preg_replace("/[^a-zA-Z0-9\s\p{P}]/", "", $sdata->description);
+    $item->setDescription(strip_tags($stripped));
     $item->setPinene(
             findStr("Pinene-", "%", $sdata->description)
     );
